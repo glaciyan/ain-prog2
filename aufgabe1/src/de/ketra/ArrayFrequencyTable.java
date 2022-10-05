b78a0eeef0adfb01468aefad39b3783d88831b5f
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-10-05 11:54:01 +0200

    init aufgabe1

---
+++ aufgabe1/src/de/ketra/ArrayFrequencyTable.java
@@ -0,0 +1,57 @@
+package de.ketra;
+
+/**
+ *
+ * @author oliverbittel
+ * @since 25.03.2021
+ */
+public class ArrayFrequencyTable extends AbstractFrequencyTable {
+	private int size;
+	private Word fqTable[];
+	private final int DEFAULT_SIZE = 100;
+
+	public ArrayFrequencyTable() {
+        clear();
+    }
+
+	@Override
+	public int size() {
+		// throw muss noch auskommentiert werden!
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		// Ihr Code:
+		// ...
+	}
+
+	@Override
+	public final void clear() {
+		// throw muss noch auskommentiert werden!
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		// Ihr Code:
+		// ...
+	}
+
+	@Override
+	public void add(String w, int f) {
+		// throw muss noch auskommentiert werden!
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		// Ihr Code:
+		// ...
+	}
+
+
+	@Override
+	public Word get(int pos) {
+		// throw muss noch auskommentiert werden!
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		// Ihr Code:
+		// ...
+	}
+
+	@Override
+	public int get(String w) {
+		// throw muss noch auskommentiert werden!
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		// Ihr Code:
+		// ...
+	}
+}
