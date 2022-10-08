577aa5ad18300431025e6f475600041c67ddbff8
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-10-08 20:30:33 +0200

    aufgabe 1 ver1

--- aufgabe1/src/de/ketra/AbstractFrequencyTable.java
+++ aufgabe1/src/de/ketra/AbstractFrequencyTable.java
@@ -18,18 +18,37 @@ public abstract class AbstractFrequencyTable implements FrequencyTable {
 
 	@Override
 	public void addAll(FrequencyTable fq) {
-		// Ihr Code:
-
+		for (int i = 0; i < fq.size(); i++) {
+			Word word = fq.get(i);
+			this.add(word.getWord(), word.getFrequency());
+		}
 	}
 
 	@Override
 	public void collectMostFrequent(FrequencyTable fq) {
-		// Ihr Code:
+		if (this.isEmpty()) return;
+		int highest = this.get(0).getFrequency();
+
+		fq.clear();
+		for (int i = 0; i < this.size(); i++) {
+			Word word = this.get(i);
+			if (word.getFrequency() < highest) break;
+
+			fq.add(word.getWord(), word.getFrequency());
+		}
 	}
 
 	@Override
 	public void collectLeastFrequent(FrequencyTable fq) {
-		// Ihr Code:
+		if (this.isEmpty()) return;
+
+		fq.clear();
+		for (int i = this.size() - 1; i >= 0; i--) {
+			Word word = this.get(i);
+			if (word.getFrequency() > 1) break;
+
+			fq.add(word.getWord(), word.getFrequency());
+		}
 	}
 
 	/**
@@ -38,8 +57,17 @@ public abstract class AbstractFrequencyTable implements FrequencyTable {
 	 */
 	@Override
 	public String toString() {
-		StringBuilder s = new StringBuilder("");
-		// Ihr Code:
+		StringBuilder s = new StringBuilder();
+
+		s.append("{");
+
+		for (int i = 0; i < this.size(); i++) {
+			s.append(this.get(i)).append(", ");
+		}
+
+		s.append("}");
+
+		s.append(" size = ").append(this.size());
 
 		return s.toString();
 	}
--- aufgabe1/src/de/ketra/ArrayFrequencyTable.java
+++ aufgabe1/src/de/ketra/ArrayFrequencyTable.java
@@ -1,57 +1,93 @@
 package de.ketra;
 
+import java.util.Arrays;
+
 /**
- *
  * @author oliverbittel
  * @since 25.03.2021
  */
 public class ArrayFrequencyTable extends AbstractFrequencyTable {
-	private int size;
-	private Word fqTable[];
-	private final int DEFAULT_SIZE = 100;
+    private int size;
+    private Word[] fqTable;
+    private static final int DEFAULT_SIZE = 100;
 
-	public ArrayFrequencyTable() {
+    public ArrayFrequencyTable() {
         clear();
     }
 
-	@Override
-	public int size() {
-		// throw muss noch auskommentiert werden!
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-		// Ihr Code:
-		// ...
-	}
+    @Override
+    public int size() {
+        return size;
+    }
 
-	@Override
-	public final void clear() {
-		// throw muss noch auskommentiert werden!
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-		// Ihr Code:
-		// ...
-	}
+    @Override
+    public final void clear() {
+        size = 0;
+        fqTable = new Word[DEFAULT_SIZE];
+    }
 
-	@Override
-	public void add(String w, int f) {
-		// throw muss noch auskommentiert werden!
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-		// Ihr Code:
-		// ...
-	}
+    @Override
+    public void add(String w, int f) {
+        int i;
+        for (i = 0; i < size; i++) {
+            Word word = fqTable[i];
+            if (word.getWord().equals(w)) {
+                word.addFrequency(f);
+
+                sortedAdd(i, word);
+
+                return;
+            }
+        }
+
+        if (fqTable.length == size) {
+            fqTable = Arrays.copyOf(fqTable, fqTable.length * 2);
+        }
+
+        sortedAdd(size, new Word(w, f));
+        size++;
+    }
+
+    /**
+     * @param i    index wo das Wort hinein sollte, von dem index wird nach links sortiert
+     * @param word das word
+     */
+    private void sortedAdd(int i, Word word) {
+        int j = i - 1;
+        // wenn es ein word links gibt und die freq kleiner ist
+        if (j >= 0 && fqTable[j].getFrequency() < word.getFrequency()) {
+            for (; j >= 0; j--) {
+                // verschiebe alle, welche kleiner sind
+                if (fqTable[j].getFrequency() < word.getFrequency()) {
+                    fqTable[j + 1] = fqTable[j];
+                } else break;
+            }
+        }
+
+        // j ist der index von dem word, welcher größer ist
+        // (also dem index wo wir break gemacht haben) deswegen + 1
+        fqTable[j + 1] = word;
+    }
 
 
-	@Override
-	public Word get(int pos) {
-		// throw muss noch auskommentiert werden!
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-		// Ihr Code:
-		// ...
-	}
+    @Override
+    public Word get(int pos) {
+        if (pos >= size) {
+            throw new IndexOutOfBoundsException("Can't access index %d for size %d".formatted(pos, size()));
+        }
 
-	@Override
-	public int get(String w) {
-		// throw muss noch auskommentiert werden!
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-		// Ihr Code:
-		// ...
-	}
+        return fqTable[pos];
+    }
+
+    @Override
+    public int get(String w) {
+        for (int i = 0; i < size; i++) {
+            Word word = fqTable[i];
+            if (word.getWord().equals(w)) {
+                return word.getFrequency();
+            }
+        }
+
+        return 0;
+    }
 }
