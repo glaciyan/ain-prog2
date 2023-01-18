8aa0f15f4cd3742677c54807efc6a7d26b09c57e
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-01-18 17:50:27 +0100

    fertig

--- aufgabe11-extra/Movie.java
+++ aufgabe11-extra/Movie.java
@@ -1,23 +1,22 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public record Movie(String title, List<String> actors, int year) {
     public Movie(String s) {
-        this(parseTitle(s),parseActors(s),parseYear(s));
+        this(parseTitle(s), parseActors(s), parseYear(s));
     }
 
     private static String parseTitle(String s) {
-        // ...
-        return "";
+        return s.substring(0, s.indexOf(" ("));
     }
 
     private static int parseYear(String s) {
-        // ..
-        return 0;
+        return Integer.parseInt(s.substring(s.indexOf("(") + 1, s.indexOf(")")));
     }
 
     private static List<String> parseActors(String s) {
         // ..
-        return Arrays.asList();
+        return Arrays.asList(s.substring(s.indexOf("/") + 1).split("/"));
     }
 }
